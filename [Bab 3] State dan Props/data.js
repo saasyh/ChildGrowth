01ddddd5e6a37edb50bdd5e7db70922fdf9ab3b2
saasyh <sit.saasyh@gmail.com>
2023-11-28 14:45:49 +0700

    [Bab 3] State dan Props

---
+++ [Bab 3] State dan Props/data.js
@@ -0,0 +1,99 @@
+export const CategoryList = [
+    {
+      id: 1,
+      categoryName: 'Popular',
+    },
+    {
+      id: 2,
+      categoryName: '0-4 Bulan',
+    },
+    {
+      id: 3,
+      categoryName: '4-8 Bulan',
+    },
+    {
+      id: 4,
+      categoryName: '8-12 Bulan',
+    },
+    {
+      id: 5,
+      categoryName: '1-3 Tahun',
+    },
+    {
+      id: 6,
+      categoryName: '3-5 Tahun',
+    },
+  ];
+
+  export const BlogList = [
+    {
+      id: 1,
+      title: 'BAGAIMANA KONDISI SI KECIL?',
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX2xlk0eoH_MiWbhN5VOSCBSLyOCHFdSIS4A&usqp=CAU',
+      text: "KONSULTASIKAN SEKARANG PADA DOKTER!"
+    },
+    {
+      id: 2,
+      title: 'Menjaga Kesehatan Anak Dengan Eating Clean',
+      category: 'Health',
+      image:
+        'https://yankes.kemkes.go.id/img/bg-img/gambarartikel_1659600431_993087.jpg',
+      createdAt: 'Jan 10, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 3,
+      title: 'Kenali Penyebab Speech Delay Pada Anak dan Cara Mengatasinya!',
+      category: 'Health',
+      image:
+        'https://mysiloam-api.siloamhospitals.com/public-asset/website-cms/website-cms-16734940268318357.webp',
+      createdAt: 'Jul 25, 2023',
+      totalComments: 207,
+    },
+    {
+      id: 4,
+      title: 'Resep Mpasi Untuk Anak Tumbuh Gigi',
+      category: 'Food',
+      image:
+        'https://www.mooimom.id/media/mamapedia/bpDxMfLq--main-image.JPG',
+      createdAt: 'Jul 25, 2023',
+      totalComments: 1760,
+    },
+    {
+      id: 5,
+      title: 'Jadwal Imunisasi Dasar Pada Anak',
+      category: 'Health',
+      image:
+        'https://mysiloam-api.siloamhospitals.com/public-asset/website-cms/website-cms-16814387792226037.webp',
+      createdAt: 'Jul 25, 2023',
+      totalComments: 100,
+    },
+    {
+      id: 6,
+      title: 'Kegiatan Untuk Mengasah Motorik Anak Umur 3-4 Tahun',
+      category: 'Lifestyle',
+      image:
+        'https://d24eqpince6acm.cloudfront.net/articles/Mudah%20Dilakukan%20di%20Rumah%2C%205%20Kegiatan%20Motorik%20Halus%20Anak%20Usia%203-4%20Tahun%20yang%20Bisa%20Melatih%20Keterampilan%20dan%20Kreativitas%20-%20image%201.webp',
+      createdAt: 'Jul 25, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 7,
+      title: 'Resep Masakan Pencegah Stunting Untuk Si Kecil',
+      category: 'Food',
+      image:
+        'https://image.popmama.com/content-images/post/20220922/doa-harian-anak-10png-5a81b86082e051abe0171eff35ed195d.png',
+      createdAt: 'Jul 25, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 8,
+      title: 'Panduan Cara Merawat Bayi Baru Lahir',
+      category: 'Lifestyle',
+      image:
+        'https://res.cloudinary.com/dk0z4ums3/image/upload/v1591186168/attached_image/belajar-merawat-bayi-baru-lahir-bagi-pasangan-muda-0-alodokter.jpg',
+      createdAt: 'Jul 25, 2023',
+      totalComments: 89,
+    },
+  ];
